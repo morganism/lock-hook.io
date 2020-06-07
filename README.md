b5eed7777a0fdf243c325e6fee314770356f8638
Morgan Sziraki <morgan.sziraki@hivehome.com>
2020-06-07 18:04:08 +0100

    DEV-000 keep this up to date will TO in 18 days ??

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lock-hook.io
 hook.io webhook
+
+https://hook.io/admin?owner=morganism&name=lock-hook#metrics
--- service/handler.rb
+++ service/handler.rb
@@ -1,2 +1,7 @@
 require 'json'
+require 'net/http'
+
+puts Net::HTTP.get('tech.sziraki.co.uk:8001', '/')
 puts JSON.generate(Hook['params'])
+console.log('hello logging');
+console.log("params:#{Hook['params']}")
